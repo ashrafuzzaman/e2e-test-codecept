9465e16276c5be64be09971fc9099e68b0495fd8
A.K.M. Ashrafuzzaman <ashrafuzzaman.g2@gmail.com>
2019-12-30 17:53:03 +0600

    pause works

--- step_definitions/steps.js
+++ step_definitions/steps.js
@@ -1,23 +1,24 @@
 const { I } = inject();
 
 Given(/I am loged in as "(.+)" with password "(.+)"/, async (email, password) => {
-  I.amOnPage('/');
-  I.fillField('Enter your email address', email);
-  I.click('Next');
-  I.fillField(`Password for ${email}`, password);
+  await I.amOnPage('/');
+  await I.fillField('Enter your email address', email);
+  await I.click('Next');
+  await I.fillField(`Password for ${email}`, password);
   await Promise.all([
     I.click('Login'),
     I.waitForNavigation()
   ]);
-  I.seeInCurrentUrl('/cloud/home');
-  I.see('Your content marketing performance');
+  await I.seeInCurrentUrl('/cloud/home');
+  // await I.see('Your content marketing performance');
 });
 
 Then(/Switch organization to "(.+)"/, async (organizationName) => {
+  // pause();
   let orgSwitcherSelector = { css: '.org-name-text' };
   let selectedOrg = await I.grabTextFrom(orgSwitcherSelector);
   if (selectedOrg !== organizationName) {
-    I.click(orgSwitcherSelector);
+    await I.click(orgSwitcherSelector);
     await Promise.all([
       I.click(organizationName),
       I.waitForNavigation()
@@ -26,24 +27,32 @@ Then(/Switch organization to "(.+)"/, async (organizationName) => {
 });
 
 Then(/Click on the New button on the header and click "(.+)"/, async (newItemText) => {
-  I.click('New', '.global-actions');
-  I.click(newItemText);
+  await I.click('.button-create-new');
+  await I.click(newItemText);
 });
 
 Then(/Click on "(.+)"/, async (text) => {
-  I.click(text);
+  await I.click(text);
 });
 
 Then(/Write "(.+)" into textbox with placeholder "(.+)"/, async (text, placeholder) => {
-  I.clearField(placeholder);
-  I.fillField(placeholder, text);
+  await I.clearField(placeholder);
+  await I.fillField(placeholder, text);
 });
 
 Then(/Select "(.+)" from dropdown below "(.+)"/, async (text, placeholder) => {
   pause();
-  I.click(text);
-  // ndl-Select__control
-  I.clearField(placeholder);
-  I.fillField(placeholder, text);
+  await I.click(text);
+  await I.clearField(placeholder);
+  await I.fillField(placeholder, text);
+});
+
+Then(/Select "(.+)" as Campaign Color/, async (color) => {
+  pause();
+  within('.ndl-ColorSelect-select', async () => {
+    I.fillField('input', color);
+    await I.click('.ndl-FormControl');
+    await I.click(color);
+  });
 });
 
